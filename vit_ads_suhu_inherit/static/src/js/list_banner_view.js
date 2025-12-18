efb55586d4df86347864f0678aff469f7f41f417
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-12-18 14:26:02 +0700

    image gen

--- vit_ads_suhu_inherit/static/src/js/list_banner_view.js
+++ vit_ads_suhu_inherit/static/src/js/list_banner_view.js
@@ -2,33 +2,20 @@
 
 import { registry } from "@web/core/registry";
 import { listView } from "@web/views/list/list_view";
-import { ListController } from '@web/views/list/list_controller';
+import { ListController } from "@web/views/list/list_controller";
+import { STAGES } from "./stage_config";
 
 export class BannerListRenderer extends ListController {
     static template = "vit_ads_suhu_inherit.BannerListRenderer";
-    static props = {...ListController.props};
+    static props = { ...ListController.props };
 
-    setup(){
-        super.setup()
-        console.log('inherited list view')
+    setup() {
+        super.setup();
     }
 
-    get stages(){
-        return [
-            {id:'vit.product_value_analysis', label: 'Product Value Analysis'},
-            {id:'vit.market_mapper', label:'Market Mapper'},
-            {id:'vit.audience_profiler', label:'Audience Profiler'},
-            {id:'vit.angle_hook', label:'Angle & Hook'},
-            {id:'vit.ads_copy', label:'Ads Copy'},
-            {id:'vit.script_writer', label:'Script Writer'},
-            {id:'vit.visual_concept', label:'Visual Concept'},
-            {id:'vit.compliance_checker', label:'Compliance Checker'},
-            {id:'vit.landing_page_builder', label:'Landing Page Builder'},
-            {id:'vit.campaign_builder', label:'Campaign Builder'},
-        ]
-    }
-    get imageSrc(){
-        return `/vit_ads_suhu_inherit/static/src/img/${this.props.resModel}.png`
+
+    get stages() {
+        return STAGES;
     }
 }
 
---
+++ vit_ads_suhu_inherit/static/src/js/stage_config.js
@@ -0,0 +1,19 @@
+/** @odoo-module **/
+
+export const STAGES = [
+    { id: "vit.product_value_analysis", label: "Product Value Analysis" },
+    { id: "vit.market_mapper", label: "Market Mapper" },
+    { id: "vit.audience_profiler", label: "Audience Profiler" },
+    { id: "vit.angle_hook", label: "Angle & Hook" },
+    { id: "vit.ads_copy", label: "Ads Copy" },
+    { id: "vit.image_generator", label: "Image Generator", parent: "vit.ads_copy", hidden: true },
+    { id: "vit.video_generator", label: "Video Generator", parent: "vit.ads_copy", hidden: true },
+    { id: "vit.script_writer", label: "Script Writer" },
+    { id: "vit.visual_concept", label: "Visual Concept" },
+    { id: "vit.compliance_checker", label: "Compliance Checker" },
+    { id: "vit.landing_page_builder", label: "Landing Page Builder" },
+    { id: "vit.campaign_builder", label: "Campaign Builder" },
+];
+
+export const getStageImageSrc = (resModel) =>
+    `/vit_ads_suhu_inherit/static/src/img/${resModel}.png`;
