c082b2afb0c66dafaed892454cdd538030f770d1
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-12-23 04:30:18 +0700

    "tree:invisible":true

--- vit_ads_suhu/model/hook.py
+++ vit_ads_suhu/model/hook.py
@@ -51,3 +51,5 @@ class hook(models.Model):
 
     angle_hook_id = fields.Many2one(comodel_name="vit.angle_hook", string="Angle")
     ads_copy_ids = fields.One2many(comodel_name="vit.ads_copy",  inverse_name="hook_id",  string=_("Ads Copy"))
+    audience_profiler_id = fields.Many2one(comodel_name="vit.audience_profiler", related="angle_hook_id.audience_profiler_id",  string=_("Audience Profiler"))
+    product_value_analysis_id = fields.Many2one(comodel_name="vit.product_value_analysis", related="angle_hook_id.product_value_analysis_id",  string=_("Product Value Analysis"))
---
+++ vit_ads_suhu_inherit/model/hook.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+class hook(models.Model):
+
+    _name = "vit.hook"
+    _inherit = "vit.hook"
+
+
+    def action_generate(self, ):
+        pass
+
+    # def _get_default_prompt(self):
+    #     prompt = self.env.ref("vit_ads_suhu_inherit.gpt_ads_copy", raise_if_not_found=False)
+    #     if prompt:
+    #         return prompt.id
+    #     return self.env["vit.gpt_prompt"].search(
+    #         [("name", "=", "ads_copy")], limit=1
+    #     ).id
+    
+    # gpt_prompt_id = fields.Many2one(comodel_name="vit.gpt_prompt",  string=_("GPT Prompt"), default=_get_default_prompt)
+    
+    @api.onchange("angle_hook_id")
+    def _get_input(self, ):
+        """
+        {
+        }
+        """
+        for rec in self:
+            hook = len(rec.angle_hook_id.hook_ids)+1
+            rec.hook_no = hook
+            rec.name = f"HOOK {hook} - ANGLE {rec.angle_hook_id.angle_no} "
+            rec.input = f"""
+# ✅ ANGLE:
+---
+{rec.angle_hook_id.output}
+
+# ✅ AUDIENCE PROFILE:
+---
+{rec.audience_profiler_id.output}
+
+# ✅ PRODUCT VALUE:
+---
+{rec.product_value_analysis_id.output}
+
+"""
