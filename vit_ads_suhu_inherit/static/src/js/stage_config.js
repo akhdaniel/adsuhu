1f866cc0b801de52291672cb7a7cc8a10eecce8f
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-12-23 10:52:33 +0700

    Update stage_config.js

--- vit_ads_suhu_inherit/static/src/js/stage_config.js
+++ vit_ads_suhu_inherit/static/src/js/stage_config.js
@@ -9,9 +9,9 @@ export const STAGES = [
     { id: "vit.ads_copy", label: "Ads Copy" },
     { id: "vit.image_generator", label: "Images", parent: "vit.ads_copy", hidden: true },
     { id: "vit.video_generator", label: "Videos", parent: "vit.ads_copy", hidden: true },
-    { id: "vit.script_writer", label: "Script Writer" },
-    { id: "vit.visual_concept", label: "Visual Concept" },
-    { id: "vit.compliance_checker", label: "Compliance Checker" },
+    { id: "vit.script_writer", label: "Script Writer" , hidden: true},
+    { id: "vit.visual_concept", label: "Visual Concept" , hidden: true},
+    { id: "vit.compliance_checker", label: "Compliance Checker" , hidden: true},
     { id: "vit.landing_page_builder", label: "Landing Page Builder" },
     { id: "vit.campaign_builder", label: "Campaign Builder" },
 ];
