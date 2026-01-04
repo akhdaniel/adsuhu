a6017a8847e8aea4c302dec1d7d4bcd8809474d2
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2026-01-04 16:46:08 +0700

    Update image_variant.py

--- vit_ads_suhu_inherit/model/image_variant.py
+++ vit_ads_suhu_inherit/model/image_variant.py
@@ -40,7 +40,7 @@ class image_variant(models.Model):
     def _get_image_url(self, ):
         base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url') or ''
         for rec in self:
-            rec.image_url = f'{base_url}/web/image/vit.image_variant/{rec.id}/image_512?unique={int(time.time())}'
+            rec.image_url = f'{base_url}/web/image/vit.image_variant/{rec.id}/image_1024?unique={int(time.time())}'
 
     def action_post_linkedin(self, ):
         _logger.info(f"----- {__name__}")
