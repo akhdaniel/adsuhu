345047c0c8947458f8a09d2612f2f5997e5b7cc3
ridhompra <ridhomp29@gmail.com>
2026-02-06 01:43:52 +0700

    [FEAT] Fetch Product Description [Shopee, Tokopedia]

---
+++ vit_ads_suhu_fetch_product_url/__manifest__.py
@@ -0,0 +1,20 @@
+#-*- coding: utf-8 -*-
+
+{
+    "name": "Ads Suhu - Fetch Product URL (TLS)",
+    "version": "1.0",
+    "depends": [
+        "base",
+        "vit_ads_suhu_inherit",
+    ],
+    "author": "Akhmad Daniel Sembiring",
+    "category": "Utility",
+    "website": "http://vitraining.com",
+    "license": "OPL-1",
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+    "odooVersion": 18
+}
