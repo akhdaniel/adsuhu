5d29ae89e38ffe9d4fb37fb7e9f2c19b2d15f341
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2026-02-13 08:26:10 +0700

    Update main.py

--- vit_xendit/controllers/main.py
+++ vit_xendit/controllers/main.py
@@ -123,6 +123,7 @@ class XenditController(http.Controller):
                 _logger.info(f'forward to bootcamp...{target_url}')
                 forward_token = token or cfg.get("webhook_token")
                 headers = {"x-callback-token": forward_token} if forward_token else {}
+                headers.update({"Content-Type": "application/json"})
                 _logger.info(f"forward_token={forward_token}")
                 _logger.info(f"payload={payload}")
                 resp = requests.post(
