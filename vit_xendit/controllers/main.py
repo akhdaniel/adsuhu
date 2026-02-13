7667bf91ae7a521140329ac652a25df9c5ffbf86
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2026-02-13 09:01:11 +0700

    Update main.py

--- vit_xendit/controllers/main.py
+++ vit_xendit/controllers/main.py
@@ -132,7 +132,7 @@ class XenditController(http.Controller):
                 _logger.info(f"payload={payload}")
                 resp = requests.post(
                     target_url,
-                    data=payload,
+                    json=json.dumps(payload),
                     headers=headers,
                     timeout=10,
                 )
